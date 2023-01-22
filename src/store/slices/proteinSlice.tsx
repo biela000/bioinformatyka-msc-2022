8082b220c8f846d762e95c2f01fce01454b38570
Bruhtek <bartosz.kas2005@gmail.com>
2023-01-22 12:43:22 +0100

    Fix a few splitting bugs

--- src/store/slices/proteinSlice.tsx
+++ src/store/slices/proteinSlice.tsx
@@ -3,11 +3,11 @@ import { Codon } from '../../types/proteinTypes';
 import { AminoAcidMap } from '../../utils/aminoAcids';
 
 // TODO: Check the translation validity
-const translate = (rna: string): [string[], string[], string[]] => {
+const splitProteins = (rna: string): [string[], string[], string[]] => {
 	const result: [string[], string[], string[]] = [[], [], []];
 	// 3 translations for 3 possible starts
 	for (let i = 0; i < 3; i++) {
-		for (let o = 0; o + 2 < rna.length; o += 3) {
+		for (let o = 0; o + i + 2 < rna.length; o += 3) {
 			// Get 3 letters from RNA
 			const codon = rna.slice(o + i, o + i + 3);
 			result[i].push(codon);
@@ -15,6 +15,16 @@ const translate = (rna: string): [string[], string[], string[]] => {
 	}
 	return result;
 };
+const translateProteins = (splitProteins: string[]) : Codon[] => {
+	return splitProteins.map(
+		codon => {
+			return {
+				threeLetterCode: codon,
+				aminoAcidLetter: AminoAcidMap[codon],
+			};
+		},
+	);
+}
 
 type ProteinState = {
 	possibleProteins?: [Codon[], Codon[], Codon[]];
@@ -26,23 +36,16 @@ export const proteinSlice = createSlice({
 	initialState: initialState,
 	reducers: {
 		addProtein: (state, action) => {
-			//TODO: Add code to protein translation, replace codons with specific amino acids, pick proteins from the result
+			//TODO: pick proteins from the result
 
 			// Replace T with U to make RNA out of potential DNA
 			const rna = action.payload.replace(/T/g, 'U');
 
 			// state = array of arrays of potential proteins
-			const possibileProteinsStrings = translate(rna);
+			const possibileProteinsStrings = splitProteins(rna);
 			state.possibleProteins = [[], [], []];
 			for (let i = 0; i < possibileProteinsStrings.length; i++) {
-				state.possibleProteins[i] = possibileProteinsStrings[i].map(
-					codon => {
-						return {
-							threeLetterCode: codon,
-							aminoAcidLetter: AminoAcidMap[codon],
-						};
-					},
-				);
+				state.possibleProteins[i] = translateProteins(possibileProteinsStrings[i]);
 			}
 		},
 		deleteProtein: (state, action) => {

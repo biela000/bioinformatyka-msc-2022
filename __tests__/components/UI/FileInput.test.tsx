082af1da119369b6ed9bfbadbd908138f3e599db
grzesta05 <43935281+grzesta05@users.noreply.github.com>
2023-01-27 08:24:57 +0100

    add tests for FileInput

---
+++ __tests__/components/UI/FileInput.test.tsx
@@ -0,0 +1,33 @@
+//Ewa, ja,
+import {render,screen} from "@testing-library/react";
+import FileInput from "../../../src/components/UI/FileInput/FileInput";
+import userEvent from "@testing-library/user-event";
+
+const testID = "file-input"
+
+describe("FileInput", ()=>{
+	it("should reject extensions other than mentioned", ()=>{
+		//With (If extension is not accepted, onInput function is not called)
+		const file1 = new File(["lorem"], "lorem.png", { type: "image/png" })
+		const mock = jest.fn()
+		//When
+		render(<FileInput dataTestID={testID} acceptedExtensions={[".txt"]} onInput={mock}/>)
+		const input = screen.getByTestId(testID) as HTMLInputElement
+
+		userEvent.upload(input, file1)
+
+		expect(mock).not.toHaveBeenCalled()
+	})
+	it("should accept extensions mentioned", ()=>{
+		//With  (If extension is accepted, onInput function is called)
+		const file1 = new File(["lorem"], "lorem.txt", { type: "text/plain" })
+		const mock = jest.fn()
+		//When
+		render(<FileInput dataTestID={testID} acceptedExtensions={[".txt"]} onInput={mock}/>)
+		const input = screen.getByTestId(testID) as HTMLInputElement
+
+		userEvent.upload(input, file1)
+
+		expect(mock).toHaveBeenCalled()
+	})
+})

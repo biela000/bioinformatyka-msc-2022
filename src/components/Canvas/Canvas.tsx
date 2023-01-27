64bf0628dd110f254df312241934e1cc33587e7a
Bruhtek <bartosz.kas2005@gmail.com>
2023-01-27 19:45:35 +0100

    Add a pan and zoom functionality to the canvas

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -6,21 +6,7 @@ function Canvas() {
 	const canvasRef = useRef<HTMLCanvasElement>(null);
 	useEffect(() => {
 		const canvas = canvasRef.current!;
-		const context = canvas.getContext("2d")!;
-		resetCanvas(context);
-
-		// const acidString = "AACCGG";
-		const acidString = "ACDGILSUV";
-		drawPeptide(context, acidString);
-
-		// drawNH3(context, 100, 100);
-		// let [x, y] = drawBase(context, 100, 100, false);
-		// drawNH(context, x, y, false);
-		// [x, y] = drawBase(context, x, y, true);
-		// drawNH(context, x, y, true);
-		// [x, y] = drawBase(context, x, y, false);
-		// drawOMinus(context, x, y);
-
+		manageCanvas(canvas, 1900, 700, "ACDGILSUV");
 	}, []);
 
 	return (
@@ -29,7 +15,8 @@ function Canvas() {
 			<canvas
 				ref={canvasRef}
 				style={{
-					border: "1px solid white"
+					border: "1px solid black",
+					cursor: "grab",
 				}}
 				id="canvas"
 				width="1900"
@@ -37,6 +24,63 @@ function Canvas() {
 			></canvas>
 		</div>
 	);
+}
+
+const manageCanvas = (canvas: HTMLCanvasElement, width: number, height: number, peptide: string) => {
+	const ctx = canvas.getContext("2d")!;
+	const cameraOffset = { x: 0, y: 0 };
+	let cameraZoom = 1;
+	const SCROLL_SENSITIVITY = 0.0005;
+	const minZoom = 0.1;
+	const maxZoom = 5;
+
+	function draw() {
+		canvas.width = width;
+		canvas.height = height;
+
+		ctx.translate(width/2, height/2);
+		ctx.scale(cameraZoom, cameraZoom);
+		ctx.translate(-width/2 + cameraOffset.x, -height/2 + cameraOffset.y);
+
+		resetCanvas(ctx);
+		drawPeptide(ctx, peptide);
+
+		requestAnimationFrame(draw);
+	}
+
+	let panning = false;
+	let panStart = { x: 0, y: 0 };
+
+	function mouseDown(e: MouseEvent) {
+		panning = true;
+		panStart.x = e.clientX/cameraZoom - cameraOffset.x;
+		panStart.y = e.clientY/cameraZoom - cameraOffset.y;
+	}
+	function mouseUp(e: MouseEvent) {
+		panning = false;
+	}
+	function mouseMove(e: MouseEvent) {
+		if(panning) {
+			cameraOffset.x = e.clientX/cameraZoom - panStart.x;
+			cameraOffset.y = e.clientY/cameraZoom - panStart.y;
+		}
+	}
+
+	function adjustZoom(delta: number) {
+		// don't zoom if panning, causes weird behaviour
+		if(panning) return;
+
+		cameraZoom += delta;
+		if(cameraZoom < minZoom) cameraZoom = minZoom;
+		if(cameraZoom > maxZoom) cameraZoom = maxZoom;
+	}
+
+	canvas.addEventListener("mousedown", mouseDown)
+	canvas.addEventListener("mouseup", mouseUp)
+	canvas.addEventListener("mousemove", mouseMove)
+	canvas.addEventListener( 'wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY))
+
+	draw();
 }
 
 const resetCanvas = (ctx: CanvasRenderingContext2D) => {
